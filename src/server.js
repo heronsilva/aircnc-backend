e557c6798033c1d35755bc866aaee1d2d942a2c7
Heron Silva <heron.code@outlook.com>
2019-10-13 23:55:11 -0300

    Implemented websocket support

--- src/server.js
+++ src/server.js
@@ -2,14 +2,33 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const routes = require('./routes')
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 
 mongoose.connect('', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query
+
+  connectedUsers[user_id] = socket.id
+})
+
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+
+  return next()
 })
 
 app.use(cors())
@@ -17,4 +36,4 @@ app.use(express.json())
 app.use(routes)
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-app.listen(3000)
+server.listen(3000)
