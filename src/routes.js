267bd95dadd380256534b97cb99176299ece2dae
Heron Silva <heron.code@outlook.com>
2019-10-14 00:02:34 -0300

    Implemented the booking requests management

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const BookingController = require('./controllers/BookingController')
 const DashboardController = require('./controllers/DashboardController')
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const ApprovalController = require('./controllers/ApprovalController')
+const RejectionController = require('./controllers/RejectionController')
 
 const routes = express.Router()
 const upload = multer(uploadConfig)
@@ -18,5 +20,8 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 routes.get('/dashboard', DashboardController.show)
 
 routes.post('/spots/:id/bookings', BookingController.store)
+
+routes.post('/bookings/:booking_id/approval', ApprovalController.store)
+routes.post('/bookings/:booking_id/rejection', RejectionController.store)
 
 module.exports = routes
